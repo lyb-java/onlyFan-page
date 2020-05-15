6553d744c748064ae95a69f740ec4f9be0a97663
lyb-java <1260150640@qq.com>
2020-05-15 17:27:40 +0800

    用户列表

--- src/view/system/sysUserAll.vue
+++ src/view/system/sysUserAll.vue
@@ -1,10 +1,30 @@
 <template>
-  <Table stripe :columns="columns" :loading="loading" :data="tableData"></Table>
+  <div>
+    <Table stripe :columns="columns" :loading="loading" :data="tableData"/>
+  <div style="text-align: right; margin-top: 5px">
+    <Page :total="total" :page-size="pageSize" show-elevator show-total @on-change="e=>{pageSearch(e)}"/>
+  </div>
+  </div>
 </template>
 <script>
   export default {
     data () {
       return {
+        PAGE_INDEX: 1,
+        /* 分页total属性绑定值 */
+        total: 0,
+        /* loading */
+        loading: false,
+        searchLoading: false,
+        /* pageInfo实体 */
+        pageIndex: 1,
+        pageSize: 10,
+        /* 查询条件 */
+        condition: {
+          processType: null,
+          updateBeginTime: null,
+          updateEndTime:null
+        },
         /* 菜单信息列表数据 */
         tableData:[],
         columns: [
@@ -22,6 +42,13 @@
           },
         ],
       }
+    },
+    methods:{
+      /** 切换页码 */
+      pageSearch (e) {
+        this.pageIndex = e
+        this.getTable()
+      },
     }
   }
 </script>
